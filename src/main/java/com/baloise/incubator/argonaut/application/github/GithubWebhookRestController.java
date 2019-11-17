5a4aab88514af8c00fb30b52889964eb0fbb7c01
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2019-11-17 09:38:43 +0100

    Add timestamp

--- src/main/java/com/baloise/incubator/argonaut/application/github/GithubWebhookRestController.java
+++ src/main/java/com/baloise/incubator/argonaut/application/github/GithubWebhookRestController.java
@@ -4,11 +4,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 @RestController
 public class GithubWebhookRestController {
 
     @PostMapping(path = "webhook/github")
     public void handleGithubWebhookEvent(@RequestBody String data) {
-        System.out.println(data);
+        System.out.println(LocalDateTime.now() + " " + data);
     }
 }
