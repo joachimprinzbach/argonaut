0a0e6d78aff97a941277e3c88390ec53cf3e92bb
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2019-11-17 08:41:55 +0100

    Initial commit

---
+++ src/main/java/com/baloise/incubator/argonaut/ArgonautApplication.java
@@ -0,0 +1,13 @@
+package com.baloise.incubator.argonaut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgonautApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgonautApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baloise/incubator/argonaut/ArgonautApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baloise.incubator.argonaut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgonautApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
