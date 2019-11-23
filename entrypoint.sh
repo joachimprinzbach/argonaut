04ab58fb4c9d7569b44d993f5a665082c90cc312
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2019-11-23 16:34:46 +0100

    Try to fix deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 exec java ${JAVA_OPTS} \
 -Djava.security.egd=file:/dev/./urandom \
+-DTEMP_FOLDER=/temp \
 -XX:+UnlockExperimentalVMOptions \
 -XX:+UseCGroupMemoryLimitForHeap \
 -jar "/opt/app/app.jar" "$@"
