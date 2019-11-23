555e5f121002a0380e4ea87eb1af36de0f28c6b6
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2019-11-23 16:40:16 +0100

    Revert "Try to fix deployment"
    This reverts commit 04ab58fb

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 exec java ${JAVA_OPTS} \
 -Djava.security.egd=file:/dev/./urandom \
--DTEMP_FOLDER=/temp \
 -XX:+UnlockExperimentalVMOptions \
 -XX:+UseCGroupMemoryLimitForHeap \
 -jar "/opt/app/app.jar" "$@"
